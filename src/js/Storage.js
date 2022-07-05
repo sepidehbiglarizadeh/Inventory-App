f316fd96a07f3a507db416e87003f2b73a643d92
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2022-07-05 22:03:21 +0430

    Storage class completed

---
+++ src/js/Storage.js
@@ -0,0 +1,54 @@
+class Storage {
+  static getAllCategories() {
+    const savedCategories = JSON.parse(localStorage.getItem("category")) || [];
+    return savedCategories.sort((a, b) => {
+      return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+    });
+  }
+
+  static saveCategories(categoryToSave) {
+    const savedCategories = Storage.getAllCategories();
+    const existedItem = savedCategories.find((c) => c.id === categoryToSave.id);
+    if (existedItem) {
+      existedItem.title = categoryToSave.title;
+      existedItem.description = categoryToSave.description;
+    } else {
+      categoryToSave.id = new Date().getTime();
+      categoryToSave.createdAt = new Date().toDateString();
+      savedCategories.push(categoryToSave);
+    }
+    localStorage.setItem("category", JSON.stringify(savedCategories));
+  }
+
+  static getAllProducts(sort = "newest") {
+    const savedProducts = JSON.parse(localStorage.getItem("product")) || [];
+    return savedProducts.sort((a, b) => {
+      if (sort === "newest") {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+      } else if (sort === "oldest") {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+      }
+    });
+  }
+
+  static saveProducts(productToSave) {
+    const savedProducts = Storage.getAllProducts();
+    const existedItem = savedProducts.find((c) => c.id === productToSave.id);
+    if (existedItem) {
+      existedItem.title = productToSave.title;
+      existedItem.quantity = productToSave.quantity;
+      existedItem.category = productToSave.category;
+    } else {
+      productToSave.id = new Date().getTime();
+      productToSave.createdAt = new Date().toDateString();
+      savedProducts.push(productToSave);
+    }
+    localStorage.setItem("product", JSON.stringify(savedProducts));
+  }
+
+  static deleteProduct(id) {
+    const savedProducts = Storage.getAllProducts();
+    const filteredProducts = savedProducts.filter((p) => p.id !== parseInt(id));
+    localStorage.setItem("product", JSON.stringify(filteredProducts));
+  }
+}
